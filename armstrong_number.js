6faaa996fcff89754700a6576920930061bfe557
KRUTIKHIRAPARA <krutik2511@gmail.com>
2023-01-27 17:03:59 +0530

    Armstrong Number Program Added..

---
+++ armstrong_number.js
@@ -0,0 +1,27 @@
+// Armstrong Number
+
+// Logic :- 153 - 1*1*1 + 5*5*5 + 3*3*3 = 153
+
+
+
+
+var checkNumber = 123;
+var temp = checkNumber;
+var noOfDigit = checkNumber.toString().length;
+var filterAns = 0;
+
+while(temp > 0)
+{
+    var digit = temp%10 ;
+    filterAns += digit**noOfDigit;
+    temp = parseInt(temp/10);
+}
+
+if(filterAns == checkNumber)
+{
+    console.log("This is Armstong Number...");
+}
+else
+{
+    console.log("This is Not A Armstong Number...");
+}
